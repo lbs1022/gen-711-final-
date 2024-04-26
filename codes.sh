5a964d09991f82c6ca8ad1c2815678ff10811d7a
matthewfarb <157921407+matthewfarb@users.noreply.github.com>
2024-04-26 15:24:22 -0400

    Update codes.sh

--- codes.sh
+++ codes.sh
@@ -1,2 +1,11 @@
-Here will be all our codes as an .sh file 
+#! /bin/bash
 
+#Prepped directories and obtained data#
+mkdir gen-711-final-project
+cd gen-711-final-project
+mkdir raw_reads
+cp /tmp/gen711_project_data/genome-assembly-fqs/69_S8_L001_R1_001.fastq.gz raw_reads
+cp /tmp/gen711_project_data/genome-assembly-fqs/69_S8_L001_R2_001.fastq.gz raw_reads
+cp /tmp/gen711_project_data/genome-assembly-fqs/777_S1_L001_R1_001.fastq.gz raw_reads
+cp /tmp/gen711_project_data/genome-assembly-fqs/777_S1_L001_R2_001.fastq.gz raw_reads
+mkdir fastqc_output
