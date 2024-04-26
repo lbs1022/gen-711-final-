1c954646e1f6c127124cfd8d9e5da52fb578c609
matthewfarb <157921407+matthewfarb@users.noreply.github.com>
2024-04-26 15:29:10 -0400

    Update codes.sh

--- codes.sh
+++ codes.sh
@@ -6,6 +6,9 @@ cd gen-711-final-project
 mkdir raw_reads
 cp /tmp/gen711_project_data/genome-assembly-fqs/69_S8_L001_R1_001.fastq.gz raw_reads
 cp /tmp/gen711_project_data/genome-assembly-fqs/69_S8_L001_R2_001.fastq.gz raw_reads
-cp /tmp/gen711_project_data/genome-assembly-fqs/777_S1_L001_R1_001.fastq.gz raw_reads
-cp /tmp/gen711_project_data/genome-assembly-fqs/777_S1_L001_R2_001.fastq.gz raw_reads
+cp /tmp/gen711_project_data/genome-assembly-fqs/15_S2_L001_R1_001.fastq.gz raw_reads
+cp /tmp/gen711_project_data/genome-assembly-fqs/15_S2_L001_R2_001.fastq.gz raw_reads
 mkdir fastqc_output
+
+#Run FastQC on raw reads#
+cd gen-711-final
